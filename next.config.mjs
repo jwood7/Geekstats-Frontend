9dc882bf1906db01f1cf6e00967d6bc2246477b1
Jacob Wood <jwood7@stevens.edu>
2024-09-24 14:50:57 -0400

    FIX: Added allowed origins to support nginx redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        serverActions: {
+          allowedOrigins: [
+            'localhost:3000', // localhost
+            'gfstats.duckdns.org', // Codespaces
+          ],
+        },
+      },
+};
 
 export default nextConfig;
