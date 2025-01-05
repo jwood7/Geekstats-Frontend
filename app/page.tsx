0f562e884c2992f0aa7f9d59eea767289e05265a
Jacob Wood <jwood7@stevens.edu>
2025-01-04 19:26:49 -0500

    FIX: Added teams link to home page

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ useEffect(()=>{
         <div className="flex justify-between gap-10 align-center">
           {process.env.NEXT_PUBLIC_IMAGE_URL ? <img className="max-w-xs p-2.5" src={process.env.NEXT_PUBLIC_IMAGE_URL + "/images/gf_header.gif"} alt="Geekfest"/> : <h1>Geekfest</h1>}
           <Link className="my-auto text-neutral-500" href="/">Summary</Link>
-          {/* <Link className="my-auto text-neutral-500" href="/Teams">Teams</Link> */}
+          <Link className="my-auto text-neutral-500" href="/Teams">Teams</Link>
         </div>
         <button className="pr-5" onClick={() => setOpenLoginModal(!openLoginModal)}>
           {
